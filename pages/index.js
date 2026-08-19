5b924e15a1ae5ca3fcd3b0d0f1bb76ec9c80489e
Luhalab <luca.labruzzo@gmail.com>
2026-08-19 05:49:25 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -482,6 +482,7 @@ export default function Home() {
                       )}
                     </div>
                   </div>
+                  </div>
                 );
               })}
             </section>
